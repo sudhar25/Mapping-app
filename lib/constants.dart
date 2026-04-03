96b06d0344e6285150df31b6e3959cc82c3d0b17
sudharsan <sudharsannadar1@gmail.com>
2026-04-03 23:10:37 +0530

    Improved configuration

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  // 🔹 Change this to your machine's local IP when testing on a real device
+  // Run `ipconfig` (Windows) or `ifconfig` (Mac/Linux) to find it
+  // For emulator use: http://10.0.2.2:8000
+  // For real device use: http://192.168.x.x:8000
+  static const String baseUrl = 'http://10.0.2.2:8000';
+}
